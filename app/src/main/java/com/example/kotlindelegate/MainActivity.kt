44bac0eea97b7e9871bf8fa26b323de50b372bd1
yono.ap <yono.ap@icloud.com>
2023-12-17 05:49:26 +0900

    location.

--- app/src/main/java/com/example/kotlindelegate/MainActivity.kt
+++ app/src/main/java/com/example/kotlindelegate/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kotlindelegate
 
 import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,21 +15,28 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.example.kotlindelegate.ui.theme.KotlinDelegateTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import kotlin.coroutines.resumeWithException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         setContent {
             KotlinDelegateTheme {
                 // A surface container using the 'background' color from the theme
@@ -36,21 +45,29 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val scope = rememberCoroutineScope()
-                    Column(Modifier.padding(16.dp)) {
+                    val location = remember { mutableStateOf("") }
+                    Column(modifier = Modifier.padding(16.dp)) {
                         Button(onClick = {
                             scope.launch {
                                 logger.trace("launch START")
                                 runCatching {
-                                    getPermission()
+                                    val permission = getPermission()
+                                    if (permission) {
+                                        getLocation()
+                                    } else {
+                                        throw Error("denied.")
+                                    }
                                 }.onSuccess {
-                                    logger.debug("permission=$it")
+                                    location.value = "${it.latitude},${it.longitude}"
                                 }.onFailure {
                                     logger.error("getPermission", it)
+                                    location.value = "${it.message}"
                                 }
                             }
                         }) {
-                            Text(text = "get permission")
+                            Text(text = "get location")
                         }
+                        Text(text = location.value)
                     }
                 }
             }
@@ -96,6 +113,32 @@ class MainActivity : ComponentActivity() {
             )
         )
         logger.trace("suspendCancellableCoroutine END")
+    }
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private suspend fun getLocation() = suspendCancellableCoroutine { continuation ->
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            throw Error("permission denied.")
+        }
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
+            logger.debug("location={}", location)
+            if (location == null) {
+                continuation.resumeWithException(Error("location is null"))
+            } else {
+                continuation.resume(location) {
+                    logger.warn("onCancellation")
+                }
+            }
+        }
     }
 }
 
